a2bfec5d332ed9a1589ab568a2acde78a4f2173d
toasterbath0758 <toasterbath0758@gmail.com>
2025-11-26 19:56:25 +0100

    update
    added form + added button to scroll to top + added fixed header to zuidoost.html

---
+++ opdracht alles/opdracht/index.js
@@ -0,0 +1,9 @@
+const mobileNav=document.getElementById("mobileNav")
+function openMobileNav(){
+    mobileNav.classList.add("active")
+}
+function closeMobileNav(){
+    mobileNav.classList.remove("active")
+}
+
+var lastpos = 0;
